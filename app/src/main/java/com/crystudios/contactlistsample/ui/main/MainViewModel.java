5dcdb42bcd6d4e09c2b2d99b6da0a2f1e62f56a6
Daniel Andujar <daniel.ap252@gmail.com>
2019-04-02 09:59:05 -0400

    Initial Build

---
+++ app/src/main/java/com/crystudios/contactlistsample/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.crystudios.contactlistsample.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
